ec79426dcd4e1b9700eaca22f13d2478bde4ec07
Manuel Hutter <manuel@hutter.io>
2021-10-18 17:43:10 +0200

    Naively calculate attendance
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- cmd/poc/main.go
+++ cmd/poc/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strings"
+	"time"
 )
 
 func init() {
@@ -36,8 +39,26 @@ func main() {
 	sess := doLogin(url, db, login, password)
 	attendances := readAttendances(url, sess.ID, sess.UID)
 
-	for _, a := range attendances {
-		fmt.Printf("[%d] %s %8s %s\n", a.ID, a.Name, a.Action, *a.ActionDesc)
+	apd := make(map[string]time.Duration)
+	start := 0
+	if attendances[0].Action == "sign_in" {
+		// last entry was "sign in", so we can't count that one yet
+		start = 1
+	}
+	for i := start; i < len(attendances)-1; i += 2 {
+		signIn := attendances[i+1].Name.ToTime()
+		signOut := attendances[i].Name.ToTime()
+		date := signOut.Format(AttendanceDateFormat)
+		apd[date] += signOut.Sub(signIn)
+	}
+
+	rows := make([]string, 0, len(apd))
+	for date, hours := range apd {
+		rows = append(rows, fmt.Sprintf("%s %.2f", date, hours.Hours()))
+	}
+	sort.Strings(rows)
+	for _, row := range rows {
+		fmt.Println(row)
 	}
 }
 
@@ -104,7 +125,7 @@ type Attendance struct {
 
 	// Name is the entry timestamp in UTC
 	// Format: '2006-01-02 15:04:05'
-	Name string `json:"name,omitempty"`
+	Name *AttendanceTime `json:"name,omitempty"`
 
 	// Action is either "sign_in" or "sign_out"
 	Action string `json:"action,omitempty"`
@@ -115,4 +136,33 @@ type Attendance struct {
 	// * `[2, "Outside office hours"]`
 	// * `[4, "Sick / Medical Consultation"]`
 	ActionDesc *json.RawMessage `json:"action_desc,omitempty"`
+}
+
+const (
+	AttendanceDateFormat     = "2006-01-02"
+	AttendanceTimeFormat     = "15:04:05"
+	AttendanceDateTimeFormat = AttendanceDateFormat + " " + AttendanceTimeFormat
+)
+
+type AttendanceTime time.Time
+
+func (at *AttendanceTime) String() string {
+	t := time.Time(*at)
+	return t.Format(AttendanceDateTimeFormat)
+}
+func (at AttendanceTime) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, at.String())), nil
+}
+func (at *AttendanceTime) UnmarshalJSON(b []byte) error {
+	ts := bytes.Trim(b, `"`)
+	t, err := time.Parse(AttendanceDateTimeFormat, string(ts))
+	if err != nil {
+		return err
+	}
+
+	*at = AttendanceTime(t)
+	return nil
+}
+func (at *AttendanceTime) ToTime() time.Time {
+	return time.Time(*at)
 }
