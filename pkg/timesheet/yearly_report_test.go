ec8d52d5d84cad7938bff1da14cb79ff9fb646e7
Chris <github.account@chrigel.net>
2021-12-24 15:41:01 +0100

    Don't display months in yearly reports where contract didn't start
    For people that started in the middle of the current year

---
+++ pkg/timesheet/yearly_report_test.go
@@ -0,0 +1,39 @@
+package timesheet
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vshn/odootools/pkg/odoo"
+)
+
+func TestReportBuilder_getEarliestStartContractDate(t *testing.T) {
+	tests := map[string]struct {
+		givenContracts odoo.ContractList
+		expectedDate   time.Time
+		expectedFound  bool
+	}{
+		"GivenNoContracts_ThenReturnFalse": {
+			givenContracts: nil,
+			expectedFound:  false,
+		},
+		"GivenContracts_WhenStartDateExists_ThenReturnTrue": {
+			givenContracts: odoo.ContractList{
+				odoo.Contract{Start: newDateTime(t, "2021-02-04 08:00")},
+			},
+			expectedDate:  newDateTime(t, "2021-02-04 08:00").ToTime(),
+			expectedFound: true,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			r := NewReporter(nil, nil, nil, tt.givenContracts)
+			resultDate, found := r.getEarliestStartContractDate()
+			assert.Equal(t, tt.expectedFound, found)
+			if tt.expectedFound {
+				assert.Equal(t, tt.expectedDate, resultDate)
+			}
+		})
+	}
+}
